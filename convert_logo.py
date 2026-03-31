4d11efad64b4098ddcb7eaf21bc024eb0e0e7df5
zizofn1 <zizofn2006@gmail.com>
2026-03-31 01:37:53 +0100

    🙈 Mise à jour du .gitignore pour ignorer les scripts de test locaux

--- convert_logo.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import os
-
-img = Image.open('logo.png')
-# Save as ICO with common sizes
-img.save('logo.ico', format='ICO', sizes=[(16, 16), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)])
-print("Logo.ico updated successfully from logo.png")
